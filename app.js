c10930cf67e932c7be4a7f9bf23ee32dfcae2a2e
ravenusmc <mcuddy77@gmail.com>
2017-06-28 15:29:10 -0400

    restructured some folders, added css folder

--- app.js
+++ app.js
@@ -25,12 +25,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-
-
-
 //load view engine 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//Setting up public folder 
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 //Home Route
 app.get('/', function(req, res){
