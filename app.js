81d274fc5d7b77353e1c6e619bd2465e52f595b4
ravenusmc <mcuddy77@gmail.com>
2017-06-30 15:31:01 -0400

    added edit and delete functions

--- app.js
+++ app.js
@@ -79,6 +79,46 @@ app.post('/articles/add', function(req,res){
   });
 });
 
+//Load Edit Form
+app.get('/article/edit/:id', function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    res.render('edit_article', {
+      article: article
+    });
+  });
+});
+
+//Update Articles
+app.post('/articles/edit/:id', function(req,res){
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = {_id:req.params.id}
+
+  Article.update(query, article, function(err){
+    if (err){
+      console.log(err);
+      return;
+    }else {
+      res.redirect('/');
+    }
+  });
+});
+
+//Deleting route-Don't forget to add in the ajax request
+app.delete('/article/:id', function(req, res){
+    let query = {_id:req.params.id};
+
+    Article.remove(query, function(err){
+      if (err){
+        console.log(err);
+      }
+      res.send('Success!');
+    });
+});
+
 //Start Server
 app.listen(3000, function(){
   console.log('Server Started on port 3000...')
